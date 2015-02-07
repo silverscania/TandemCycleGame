ed3bf2488a1cc5b1bf251daa2fff83c802c13ae4
James Clark <silverscania@gmail,com>
2015-02-07 15:08:10 +0100

    two bikers

--- Assets/BikeAnimationController.cs
+++ Assets/BikeAnimationController.cs
@@ -3,7 +3,10 @@ using System.Collections;
 
 public class BikeAnimationController : MonoBehaviour {
 	public Transform[] rotateyBits;
+	public Transform[] xRotateyBits;
 
+	public WheelCollider backWheel;
+	protected float angle;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,10 +14,13 @@ public class BikeAnimationController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+		angle += backWheel.rpm * Time.deltaTime;
+		angle = angle % 360;
 		foreach(Transform t in rotateyBits) {
-			Vector3 current = t.localRotation.eulerAngles;
-			t.localRotation = Quaternion.Euler(0,90,Time.time*300);
+			t.localRotation = Quaternion.Euler(0,90,angle*8);
+		}
+		foreach(Transform t in xRotateyBits) {
+			t.localRotation = Quaternion.Euler(angle*360/60,0,0);
 		}
 	}
 }
