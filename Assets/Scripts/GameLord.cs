16257431b30b0cd22e5e6e5766846e8966324516
kwt <dook@aergia.dk>
2015-02-07 19:23:58 +0100

    GameLord prefab, musicplayer, GUI
    Added GameLord prefab controlling start/end game and gui.
    Added MusicPlayer doing some simple layered music depending on player
    speed.

--- Assets/Scripts/GameLord.cs
+++ Assets/Scripts/GameLord.cs
@@ -2,31 +2,87 @@
 using System.Collections;
 
 public class GameLord : MonoBehaviour {
-
+	
 	public BikeController team1;
 	public BikeController team2;
+
+	public Camera guiCamera;
+
+	public GameObject title;
+	public GameObject endText;
+
+	public TextMesh winnerName;
+	public TextMesh countDown;
 
 	public float winDistance; // If the teams are this far apart, crash the back one and the first one wins
 
 	public bool gameOver = false;
 
+	private int countDownTime = 4;
+
 	// Use this for initialization
 	void Start () {
-		
+		title.SetActive (true);
+		endText.SetActive (false);
+
+		team1.enabled = false;
+		team2.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float distance = Mathf.Abs (team1.frame.transform.localPosition.z - team2.frame.transform.localPosition.z);
+		BikeController winner = team1.frame.transform.localPosition.z > team2.frame.transform.localPosition.z ? team1 : team2;
 		if (distance >= winDistance) {
 			gameOver = true;
+			endText.SetActive(true);
+
+			team1.brakeToStop();
+			team2.brakeToStop();
+			team1.enabled = false;
+			team2.enabled = false;
+
+			if(winner.alive){
+				winnerName.text = winner.gameObject.name;
+			} else {
+				winnerName.text = "No one";
+			}
+		}
+
+		if (Input.GetButtonUp ("Fire1") && title.activeSelf) {
+			// Start the game
+			title.SetActive(false);
+			countDownGo();
+		}
+
+		if (Input.GetButtonUp ("Fire1") && gameOver) {
+			Application.LoadLevel("main 4");
 		}
 	}
 
-	void OnGUI(){
-		if (gameOver) {
-			GUI.Label (new Rect (0, Screen.height / 2, Screen.width, Screen.height), "GAME OVER! ");
+	private void count(){
+		countDownTime--;
+
+		if (countDownTime == 0) {
+			countDown.text = "GO!";
+		} else if (countDownTime == -1) {
+			countDown.text = "";
+			team1.enabled = true;
+			team2.enabled = true;
+			CancelInvoke();
+		} else {
+			countDown.text = countDownTime + "";
 		}
-		GUI.Label (new Rect (0, Screen.height / 2 + 20, Screen.width, Screen.height), Time.time + "  " + Mathf.Abs (team1.frame.transform.localPosition.z - team2.frame.transform.localPosition.z));
+	}
+
+	private void countDownGo(){
+		InvokeRepeating("count", 1f, 1f);
+	}
+
+	void OnGUI(){
+//		if (gameOver) {
+//			GUI.Label (new Rect (0, Screen.height / 2, Screen.width, Screen.height), "GAME OVER! ");
+//		}
+//		GUI.Label (new Rect (0, Screen.height / 2 + 20, Screen.width, Screen.height), Time.time + "  " + Mathf.Abs (team1.frame.transform.localPosition.z - team2.frame.transform.localPosition.z));
 	}
 }
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	public BikeController teamOne;
+	public BikeController teamTwo;
+
+	public AudioSource[] tracks;
+
+	public float maxSpeed;
+	public float musicVolume = 1;
+	private float fastestVelocity;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		fastestVelocity = Mathf.Max (teamOne.frame.velocity.magnitude, teamTwo.frame.velocity.magnitude);
+		for (int n = 0 ; n < tracks.Length ; n++) {
+			if(fastestVelocity > (maxSpeed / tracks.Length) * (n)){
+				//tracks[n].volume = 1;
+				if(n == 1){
+					tracks[n].volume = musicVolume;
+				} else {
+					tracks[n].volume = musicVolume;
+				}
+				if(n > 0) tracks[n-1].volume = 0;
+			} else {
+				tracks[n].volume = 0;
+			}
+		}
+	}
+}
