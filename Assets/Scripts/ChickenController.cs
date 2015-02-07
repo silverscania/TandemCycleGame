ae457a49a3a7730a0c391bc3ec502b1418110eb6
iliana <ipavlova43@gmail.com>
2015-02-07 14:39:24 +0100

    chicken sounds & collider

---
+++ Assets/Scripts/ChickenController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChickenController : MonoBehaviour {
+
+	public AudioClip[] crashSounds;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//make sound on collision
+	void OnCollisionEnter(Collision collision) {
+		int randomSound = Random.Range(0, crashSounds.Length);
+		audio.PlayOneShot(crashSounds[randomSound], 1.0f);
+		audio.Play();
+
+		rigidbody.constraints = RigidbodyConstraints.None;
+	}
+}
