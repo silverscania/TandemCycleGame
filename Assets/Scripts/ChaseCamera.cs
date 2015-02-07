41439b1f6c338c4e2a0d807f0078b83558bfd7ef
James Clark <silverscania@gmail,com>
2015-02-07 11:48:09 +0100

    chase camera and chicken stuff

--- Assets/Scripts/ChaseCamera.cs
+++ Assets/Scripts/ChaseCamera.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class ChaseCamera : MonoBehaviour {
 
-	public Transform target;
+	public Transform target1, target2;
 	public Vector3 offset;
+	public float minZoom, maxZoom, zoomRange;
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +14,25 @@ public class ChaseCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = target.transform.position + offset;
+		transform.position = offset;
+
+		Transform target;// = target1.transform.position.x < target2.transform.position.x ? target1 : target2;
+		Transform behind; 
+		if(target1.transform.position.x < target2.transform.position.x) {
+			target = target1;
+			behind = target2;
+		}
+		else {
+			target = target2;
+			behind = target1;
+		}
+		transform.position = new Vector3(
+			(behind.transform.position.x + target.transform.position.x*3)/4.0f + offset.x,
+			offset.y,
+			offset.z);
+
+		float distance = Mathf.Abs(target1.transform.position.x - target2.transform.position.x);
+		camera.orthographicSize = Mathf.Lerp(minZoom, maxZoom, distance/zoomRange);
 		//transform.LookAt(target);
 	}
 }
