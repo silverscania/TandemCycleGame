361119b430b42da94ddcec3e7888280dccd38129
kwt <dook@aergia.dk>
2015-02-07 12:10:22 +0100

    Added joy controls for two players

--- Assets/Scripts/BikeController.cs
+++ Assets/Scripts/BikeController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class BikeController : MonoBehaviour {
 
+	public bool playerTwo;
+
 	public WheelCollider frontWheel, backWheel1, backWheel2;
 	public Rigidbody frame, balanceWeight;
 	public GameObject mark;
@@ -11,6 +13,7 @@ public class BikeController : MonoBehaviour {
 	public float maxSteerAngle = 10.0f;
 
 	// Joy input
+
 	private float lastAngleRight;    // Angle at the last update
 	private float deltaAngleRight;
 	private float currentAngleRight;
@@ -19,45 +22,20 @@ public class BikeController : MonoBehaviour {
 	private float deltaAngleLeft;
 	private float currentAngleLeft;
 
+	private string playerName = "P1";
+
 	// Use this for initialization
 	void Start () {
 		mark = GameObject.Find("Mark");
+
+		if (playerTwo) {
+			playerName = "P2";
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float speed = frame.rigidbody.velocity.magnitude;
-
-		//Get desired angle based on rotational speed
-		//float rotSpeed = frame.angularVelocity.y;//*Time.deltaTime;
-		//float bankAngle = -rotSpeed*3; //*speed;
-
-		//mark.gameObject.transform.localRotation = Quaternion.Euler(0,0,bankAngle);
-		//mark.transform.rotation = gameObject.transform.rotation;// * Quaternion.Euler(0, 0, 0);
-		//Debug.Log(bankAngle);
-
-		//float rotation = Mathf.Clamp(frame.rigidbody.rotation.z-bankAngle, -30, 30);
-		//float torque = -rotation*20*speed; //0*speed;//*Time.deltaTime;
-
-		//balanceWeight.gameObject.transform.localPosition = new Vector3(torque, 0, 0);
-
-
-		//torque = Mathf.Min(100, torque);
-		//frame.AddTorque(new Vector3(0,0, torque));
-	//	frame.rigidbody.inertiaTensorRotation = Quaternion.Euler(0,0,0);
-	//	frame.rigidbody.angularVelocity = new Vector3();
-
-		if (Input.GetKey(KeyCode.W)) {
-			if (speed < maxSpeed) {
-				setTorqueAndBrake(10, 0);
-			}
-			else {
-				setTorqueAndBrake(0,0);
-			}
-		}
-		else {
-			setTorqueAndBrake(0, 20);
-		}
 
 		//JointSpring hingeSpring = balanceWeight.hingeJoint.spring;
 		if(Input.GetKey(KeyCode.A)) {
@@ -73,33 +51,51 @@ public class BikeController : MonoBehaviour {
 		//	hingeSpring.targetPosition = 0;
 		}
 
-		//frame.rigidbody.AddRelativeTorque(0,0, -frontWheel.steerAngle*3);
-		//frame.rigidbody.centerOfMass = new Vector3(-frontWheel.steerAngle, 0, 0);
-	
-		//balanceWeight.hingeJoint.spring = hingeSpring;
-
-		frontWheel.steerAngle = Mathf.Clamp(frontWheel.steerAngle, -maxSteerAngle, maxSteerAngle);
-
-
 		// Joystick input
-		Vector3 rotationLeft = new Vector3 (0, Mathf.Atan2 (Input.GetAxis ("LeftVertical"), Input.GetAxis ("LeftHorizontal")) * -180 / Mathf.PI, 0);
-		Vector3 rotationRight = new Vector3 (0, Mathf.Atan2 (Input.GetAxis ("RightVertical"), Input.GetAxis ("RightHorizontal")) * -180 / Mathf.PI, 0);
+		Vector3 rotationLeft = new Vector3 (0, Mathf.Atan2 (Input.GetAxis (playerName + "LeftVertical"), Input.GetAxis (playerName + "LeftHorizontal")) * -180 / Mathf.PI, 0);
+		Vector3 rotationRight = new Vector3 (0, Mathf.Atan2 (Input.GetAxis (playerName + "RightVertical"), Input.GetAxis (playerName + "RightHorizontal")) * -180 / Mathf.PI, 0);
 
 		currentAngleLeft = rotationLeft.y;
 		currentAngleRight = rotationRight.y;
 
-//		if (Mathf.RoundToInt (currentAngleLeft) != Mathf.RoundToInt (lastAngleLeft)) {
-//
-//			// only do something if current and last angle er either both positive or both negative
-//			if ((currentAngle < 0 && lastAngle < 0) ||
-//			    (currentAngle > 0 && lastAngle > 0) ) {
-//
-//				deltaAngle = lastAngle - currentAngle;
-//				//rotateAmount += deltaAngle;
-//				setTorqueAndBrake(deltaAngle, 0);
-//			}
-//		}
-//		lastAngle = currentAngle;
+		// RIGHT STICK
+		if (Mathf.RoundToInt (currentAngleRight) != Mathf.RoundToInt (lastAngleRight)) {
+			// only do something if current and last angle er either both positive or both negative
+			if ((currentAngleRight < 0 && lastAngleRight < 0) ||
+			    (currentAngleRight > 0 && lastAngleRight > 0) ) {
+				
+				deltaAngleRight = lastAngleRight - currentAngleRight;
+				//rotateAmount += deltaAngle;
+				setTorqueAndBrakeFront(deltaAngleRight/10, 0);
+			}
+		}
+
+		// LEFT STICK
+		if (Mathf.RoundToInt (currentAngleLeft) != Mathf.RoundToInt (lastAngleLeft)) {
+			if ((currentAngleLeft < 0 && lastAngleLeft < 0) ||
+			    (currentAngleLeft > 0 && lastAngleLeft > 0) ) {
+				
+				deltaAngleLeft = lastAngleLeft - currentAngleLeft;
+				//rotateAmount += deltaAngle;
+				setTorqueAndBrakeBack(deltaAngleLeft/10, 0);
+			}
+		}
+		// remember latest angles
+		lastAngleRight = currentAngleRight;
+		lastAngleLeft = currentAngleLeft;
+
+		
+		// RIGHT TRIGGER
+		float rightTrigger = Input.GetAxis (playerName + "RightTrigger");
+		frontWheel.steerAngle += steerSpeed*Time.deltaTime * rightTrigger;
+
+		// LEFT TRIGGER
+		float leftTrigger = Input.GetAxis (playerName + "LeftTrigger");
+		frontWheel.steerAngle -= steerSpeed*Time.deltaTime * leftTrigger;	
+
+		frontWheel.steerAngle = Mathf.Clamp(frontWheel.steerAngle, -maxSteerAngle, maxSteerAngle);
+
+		// gradually straighten up the bike
 	}
 
 	void OnGUI(){
