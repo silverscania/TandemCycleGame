9cbfa7ed7f87ad310b035ae0a227293ccb33219c
James Clark <silverscania@gmail,com>
2015-02-07 12:53:26 +0100

    ik and chickin prefab

---
+++ Assets/BikeAnimationController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BikeAnimationController : MonoBehaviour {
+	public Transform[] rotateyBits;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		foreach(Transform t in rotateyBits) {
+			Vector3 current = t.localRotation.eulerAngles;
+			t.localRotation = Quaternion.Euler(0,90,Time.time*300);
+		}
+	}
+}
---
+++ Assets/Scripts/AvatarIK.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AvatarIK : MonoBehaviour {
+
+	protected Animator animator;
+	
+	public bool ikActive = false;
+	public Transform rightHandObj = null;
+	
+	void Start () 
+	{
+		animator = GetComponent<Animator>();
+	}
+	
+	//a callback for calculating IK
+	void OnAnimatorIK()
+	{
+		if(animator) {
+			Debug.Log ("Animator");
+			//if the IK is active, set the position and rotation directly to the goal. 
+			if(ikActive) {
+				Debug.Log("ik active");
+				//weight = 1.0 for the right hand means position and rotation will be at the IK goal (the place the character wants to grab)
+				animator.SetIKPositionWeight(AvatarIKGoal.RightFoot,1.0f);
+				animator.SetIKRotationWeight(AvatarIKGoal.RightFoot,1.0f);
+				
+				//set the position and the rotation of the right hand where the external object is
+				if(rightHandObj != null) {
+					animator.SetIKPosition(AvatarIKGoal.RightFoot,rightHandObj.position);
+					animator.SetIKRotation(AvatarIKGoal.RightFoot,rightHandObj.rotation);
+				}                   
+				
+			}
+			
+			//if the IK is not active, set the position and rotation of the hand back to the original position
+			else {          
+				animator.SetIKPositionWeight(AvatarIKGoal.RightFoot,0);
+				animator.SetIKRotationWeight(AvatarIKGoal.RightFoot,0);             
+			}
+		}
+	}    
+}
