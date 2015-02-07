f58fca930bfa7b1ed7b8a68e2ba739e7bb60d746
James Clark <silverscania@gmail,com>
2015-02-07 17:25:22 +0100

    chicken spawnage

--- Assets/Scripts/ChickenController.cs
+++ Assets/Scripts/ChickenController.cs
@@ -4,24 +4,37 @@ using System.Collections;
 public class ChickenController : MonoBehaviour {
 
 	public AudioClip[] crashSounds;
-
+	protected float collisionTime;
 	// Use this for initialization
 	void Start () {
-		rigidbody.constraints = RigidbodyConstraints.FreezeAll;
-	
+		//rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+		collider.enabled = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(collider.enabled == false && Time.time > collisionTime + 5) {
+			Destroy(gameObject);
+		}
+		if(transform.position.y < -5 || transform.position.y > 10) {
+			Destroy(gameObject);
+		}
 	}
 
 	//make sound on collision
 	void OnCollisionEnter(Collision collision) {
-		int randomSound = Random.Range(0, crashSounds.Length);
-		audio.PlayOneShot(crashSounds[randomSound], 1.0f);
-		audio.Play();
+		Debug.Log(collision.relativeVelocity.sqrMagnitude);
+		if( collision.relativeVelocity.sqrMagnitude > 20) {
+			collider.enabled = false;
+			collisionTime = Time.time;
 
-		rigidbody.constraints = RigidbodyConstraints.None;
+			int randomSound = Random.Range(0, crashSounds.Length);
+			audio.PlayOneShot(crashSounds[randomSound], 1.0f);
+			audio.Play();
+
+			//rigidbody.constraints = RigidbodyConstraints.None;
+			//rigidbody.AddExplosionForce(100.0f, collision.transform.position, 5.0f);
+			rigidbody.AddForce(new Vector3(-20, 150, 0));
+		}
 	}
 }
--- Assets/Scripts/GeometrySpawn.cs
+++ Assets/Scripts/GeometrySpawn.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class GeometrySpawn : MonoBehaviour {
 
 	public Transform prefabToCreate;
+	public Transform[] chicken;
 	public Vector3 offset = new Vector3(20, 0, 0);
 
 	// Use this for initialization
@@ -22,5 +23,20 @@ public class GeometrySpawn : MonoBehaviour {
 		collider.enabled = false;
 		//Debug.Log("hey");
 		Instantiate(prefabToCreate, transform.parent.transform.position + offset, Quaternion.identity);
+
+		//Spawn random chicken flocks
+		int numFlocks = Random.Range(1, 2);
+		for(int flock = 0; flock < numFlocks; ++flock){
+			Vector3 flockLoc = new Vector3(
+				Random.Range(-80, 0),
+				4.5f,
+				Random.Range(-10, 10));
+
+			Instantiate(chicken[(int)Random.Range(0, chicken.Length)], 
+		            transform.parent.transform.position + 
+		            offset + 
+			        flockLoc,
+		            Quaternion.Euler(new Vector3(0, Random.Range(0, 180), 0)));
+		}
 	}
 }
