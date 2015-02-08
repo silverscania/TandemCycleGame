043b527d374e1424e8f3e810f7ef0cf26925b526
iliana <ipavlova43@gmail.com>
2015-02-08 12:27:46 +0100

    disco disco

--- Assets/Scripts/DiscoLights.cs
+++ Assets/Scripts/DiscoLights.cs
@@ -7,19 +7,19 @@ public class DiscoLights : MonoBehaviour {
 	public Rigidbody frame;
 	// Use this for initialization
 	void Start () {
-		randomColour.r = Random.Range(0.1f, 0.8f);
-		randomColour.g = Random.Range(0.1f, 0.8f);
-		randomColour.b = Random.Range(0.1f, 0.8f);
+		randomColour.r = Random.Range(0.65f, 1.0f);
+		randomColour.g = Random.Range(0.65f, 1.0f);
+		randomColour.b = Random.Range(0.65f, 1.0f);
 
-		anotherRandomColour.r = Random.Range(0.1f, 0.8f);
-		anotherRandomColour.g = Random.Range(0.1f, 0.8f);
-		anotherRandomColour.b = Random.Range(0.1f, 0.8f);
+		anotherRandomColour.r = Random.Range(0.6f, 1.0f);
+		anotherRandomColour.g = Random.Range(0.6f, 1.0f);
+		anotherRandomColour.b = Random.Range(0.65f, 1.0f);
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(frame.rigidbody.velocity.magnitude > 3) {
+		if(frame.rigidbody.velocity.magnitude > 4.5) {
 			light.enabled = true;
 			transform.localRotation = Quaternion.Euler(90, Time.time*100, 0);
 			light.color = Color.Lerp(randomColour, anotherRandomColour, Mathf.Sin(Time.time));
