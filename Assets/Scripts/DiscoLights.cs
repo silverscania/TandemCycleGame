9117d71ace6d22f8a5fe82744cc5a045c74c69ef
iliana <ipavlova43@gmail.com>
2015-02-08 12:16:55 +0100

    lights

--- Assets/Scripts/DiscoLights.cs
+++ Assets/Scripts/DiscoLights.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class DiscoLights : MonoBehaviour {
 	
 	Color randomColour, anotherRandomColour;
+	public Rigidbody frame;
 	// Use this for initialization
 	void Start () {
 		randomColour.r = Random.Range(0.1f, 0.8f);
@@ -18,14 +19,16 @@ public class DiscoLights : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-		transform.RotateAround(transform.localPosition, Vector3.up, 10.0f * Time.deltaTime);
-
-		Vector3 newPosition = new Vector3(Mathf.Sin(Time.deltaTime) * 0.005f, Mathf.Cos(Time.deltaTime) * 0.005f, 0.0f);
-		transform.Translate(newPosition);
-
-		
-		light.color = Color.Lerp(randomColour, anotherRandomColour, Mathf.Sin(Time.time));
-	
+		if(frame.rigidbody.velocity.magnitude > 3) {
+			light.enabled = true;
+			transform.localRotation = Quaternion.Euler(90, Time.time*100, 0);
+			light.color = Color.Lerp(randomColour, anotherRandomColour, Mathf.Sin(Time.time));
+		}
+		else {
+			light.enabled = false;
+		}
+		//transform.RotateAround(transform.localPosition, Vector3.up, 10.0f * Time.deltaTime);
+		//Vector3 newPosition = new Vector3(Mathf.Sin(Time.deltaTime) * 0.005f, Mathf.Cos(Time.deltaTime) * 0.005f, 0.0f);
+		//transform.Translate(newPosition);
 	}
 }
